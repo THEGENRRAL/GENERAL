5fd3e52ded7f598690f044bf956bf746c619cce7
THEGENRRAL <naharneno1988@gmail.com>
2017-06-11 14:52:03 +0300

    Add files via upload

---
+++ plugins/lock_tag.lua
@@ -0,0 +1,45 @@
+--[[ -- تم التعديل و التعريب بواسطه @KNSLTHM
+
+--[[ 
+                                         Dev @KNSLTHM
+                                         Dev @NAHAR2_BOT
+         
+       
+              CH > @NENO_CH
+--]]
+local function run(msg, matches)
+    if is_momod(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['tag'] then
+                lock_tag = data[tostring(msg.to.id)]['settings']['tag']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_tag == "yes" then
+       delete_msg(msg.id, ok_cb, true)
+    end
+end
+ 
+return {
+  patterns = {
+  "#"
+  },
+  run = run
+}
+
+
+--[[ -- تم التعديل و التعريب بواسطه @KNSLTHM
+
+--[[ 
+                                         Dev @KNSLTHM
+                                         Dev @NAHAR2_BOT
+         
+       
+              CH > @NENO_CH
+--]]
