4995901f08b449f3bed775da3053d8cd8775463b
THEGENRRAL <naharneno1988@gmail.com>
2017-06-11 14:35:41 +0300

    Add files via upload

---
+++ plugins/banbot.lua
@@ -0,0 +1,39 @@
+--[[ -- تم التعديل و التعريب بواسطه @KNSLTHM
+
+--[[ 
+                                         Dev @KNSLTHM
+                                         Dev @NAHAR2_BOT
+         
+       
+              CH > @NENO_CH
+--]]
+
+do
+
+local function run(msg, matches)
+local bot_id = our_id 
+local receiver = get_receiver(msg)
+    if matches[1] == 'غادر'  and is_admin1(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+	   leave_channel(receiver, ok_cb, false)
+    end
+end
+ 
+return {
+  patterns = {
+    "^(غادر)$",
+    "^[!#/](غادر)$",
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
+--[[ -- تم التعديل و التعريب بواسطه @KNSLTHM
+
+--[[ 
+                                         Dev @KNSLTHM
+                                         Dev @NAHAR2_BOT
+         
+       
+              CH > @NENO_CH
+--]]
